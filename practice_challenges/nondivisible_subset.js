3d2cbafc828c4d3584a6566c949b863b1ed2b8c4
Femi Agbebi <olufemiagbebi@gmail.com>
2021-06-14 19:33:41 +0100

    Complete daily code challenge

--- practice_challenges/nondivisible_subset.js
+++ practice_challenges/nondivisible_subset.js
@@ -1,9 +1,9 @@
 const nonDivisibleSet = (divisor, arr) => {
     const sets = []
-    const counter = {}
+    let subSet = []
     let start = 0
     let next = 0
-    // let maxLength = 0
+    let maxLength = 0
     while (true) {
         if (start === arr.length - 1) break
         next++
@@ -11,48 +11,68 @@ const nonDivisibleSet = (divisor, arr) => {
         const numTwo = arr[next]
         const isNotDivisible = ((numOne + numTwo) % divisor) !== 0
         if (isNotDivisible) {
-            if (!counter[numOne]) {
-                let boolOne = true
-                for (num in counter) {
-                    if (((numOne + num) % divisor) === 0) {
-                        boolOne = false
+            if (sets.length) {
+                let inSetsOne = false
+                let inSetsTwo = false
+                let isNotInArrayOne, isNotDivisibleOne, isNotInArrayTwo, isNotDivisibleTwo
+                sets.forEach(set => {
+                    isNotInArrayOne = !set.includes(numOne)
+                    isNotDivisibleOne = set.every(setNum => ((numOne + setNum) % divisor) !== 0)
+                    isNotInArrayTwo = !set.includes(numTwo)
+                    isNotDivisibleTwo = set.every(setNum => ((numTwo + setNum) % divisor) !== 0)
+
+                    if (!isNotInArrayOne) {
+                        inSetsOne = true
                     }
-                }
-                console.log(boolOne)
-                if (boolOne) {
-                    sets.push(numOne)
-                }
-                counter[numOne] = numOne
-            }
-            if (!counter[numTwo]) {
-                let boolTwo = true
-                for (num in counter) {
-                    if (((numTwo + num) % divisor) === 0) {
-                        boolTwo = false
+                    if (!isNotInArrayTwo) {
+                        inSetsTwo = true
                     }
+
+                    if (next === start + 1) {
+                        if (isNotInArrayOne && isNotDivisibleOne) {
+                            set.push(numOne)
+                        }
+                    }
+                    if (isNotInArrayTwo && isNotDivisibleTwo) {
+                        set.push(numTwo)
+                    }
+                    if (set.length > maxLength) {
+                        maxLength = set.length
+                    }
+                })
+                if (!inSetsOne) {
+                    subSet.push(numOne)
                 }
-                if (boolTwo) {
-                    sets.push(numTwo)
+                if (!inSetsTwo) {
+                    subSet.push(numTwo)
                 }
-                counter[numTwo] = numTwo
+
+            } else {
+                sets.push([numOne, numTwo])
             }
+        }
+        if (subSet.length) {
+            sets.push(subSet)
+            subSet = []
         }
         if (next === arr.length - 1) {
             start++
             next = start
         }
     }
-    console.log(sets)
-    console.log(counter)
-    return sets.length
+    return maxLength
 }
+
+// console.log(nonDivisibleSet(4, [19, 10, 12, 10, 24, 25, 22]))
+console.log(nonDivisibleSet(7, [278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436]))
+// console.log(nonDivisibleSet(3, [1, 7, 2, 4]))
+
 // const nonDivisibleSet = (divisor, arr) => {
 //     const sets = []
 //     const counter = {}
-//     let subSet = []
 //     let start = 0
 //     let next = 0
-//     let maxLength = 0
+//     // let maxLength = 0
 //     while (true) {
 //         if (start === arr.length - 1) break
 //         next++
@@ -60,37 +80,31 @@ const nonDivisibleSet = (divisor, arr) => {
 //         const numTwo = arr[next]
 //         const isNotDivisible = ((numOne + numTwo) % divisor) !== 0
 //         if (isNotDivisible) {
-//             if (sets.length) {
-//                 sets.forEach(set => {
-//                     if (next === start + 1) {
-//                         const boolOne = set.every(setNum => (numOne !== setNum && ((numOne + setNum) % divisor) !== 0))
-//                         if (boolOne && !counter[numOne]) {
-//                             counter[numOne] = numOne
-//                             set.push(numOne)
-//                         } else if (!boolOne && !counter[numOne]) {
-//                             // counter[numOne] = numOne
-//                             subSet.push(numOne)
-//                         }
+//             if (!counter[numOne]) {
+//                 let boolOne = true
+//                 for (num in counter) {
+//                     if (((numOne + num) % divisor) === 0) {
+//                         boolOne = false
 //                     }
-//                     const boolTwo = set.every(setNum => (numTwo !== setNum && ((numTwo + setNum) % divisor) !== 0))
-//                     if (boolTwo && !counter[numTwo]) {
-//                         counter[numTwo] = numTwo
-//                         set.push(numTwo)
-//                     } else if (!boolTwo && !counter[numTwo]) {
-//                         // counter[numTwo] = numTwo
-//                         subSet.push(numTwo)
-//                     }
-//                     if (set.length > maxLength) {
-//                         maxLength = set.length
-//                     }
-//                 })
-//             } else {
-//                 sets.push([numOne, numTwo])
+//                 }
+//                 console.log(boolOne)
+//                 if (boolOne) {
+//                     sets.push(numOne)
+//                 }
+//                 counter[numOne] = numOne
 //             }
-//         }
-//         if (subSet.length) {
-//             sets.push(subSet)
-//             subSet = []
+//             if (!counter[numTwo]) {
+//                 let boolTwo = true
+//                 for (num in counter) {
+//                     if (((numTwo + num) % divisor) === 0) {
+//                         boolTwo = false
+//                     }
+//                 }
+//                 if (boolTwo) {
+//                     sets.push(numTwo)
+//                 }
+//                 counter[numTwo] = numTwo
+//             }
 //         }
 //         if (next === arr.length - 1) {
 //             start++
@@ -98,12 +112,12 @@ const nonDivisibleSet = (divisor, arr) => {
 //         }
 //     }
 //     console.log(sets)
-//     return maxLength
+//     console.log(counter)
+//     return sets.length
 // }
 
-// console.log(nonDivisibleSet(4, [19, 10, 12, 10, 24, 25, 22]))
-console.log(nonDivisibleSet(7, [278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436]))
-// console.log(nonDivisibleSet(3, [1, 7, 2, 4]))
+
+
 
 // const nonDivisibleSet = (divisor, arr) => {
 //     let sets = []
