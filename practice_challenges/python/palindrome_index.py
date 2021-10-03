01594c4dbcb7f87befd577e391f38c110794afe0
Femi Agbebi <olufemiagbebi@gmail.com>
2021-10-03 17:13:11 +0100

    Solve daily code challenge

--- practice_challenges/python/palindrome_index.py
+++ practice_challenges/python/palindrome_index.py
@@ -2,44 +2,89 @@ def palindromeIndex(string_query) -> int:
     """Determine index of string removed from palindrome"""
     str_len = len(string_query)
     reversed_str = string_query[::-1]
+    removed_index = -1
+
     if string_query == reversed_str or str_len == 1:
-        return -1
+        return removed_index
 
     mid_index = int(str_len / 2)
-    # prev_index = mid_index - 1
-    # next_index = mid_index + 1
-    # mid_char = string_query[mid_index]
-    # prev_char = string_query[prev_index]
-    # next_char = string_query[next_index]
+    next_index = mid_index + 1
+    prev_index = mid_index - 1
+    is_even = str_len % 2 == 0
 
-    # is_even = str_len % 2 == 0
-    # if is_even:
-    #     if prev_char == mid_char:
-    list_str = list(string_query)
+    if is_even:
+        if string_query[prev_index] != string_query[next_index]:
+            next_index = mid_index
+            prev_index -= 1
+    else:
+        prev_index = mid_index
+        if string_query[next_index] != string_query[prev_index]:
+            prev_index = mid_index - 1
+            next_index = mid_index
 
-    start_index = 0
-    end_index = str_len - 1
     while True:
-        if start_index == mid_index or end_index == mid_index:
+        if prev_index < 0:
+            removed_index = next_index
             break
 
-        list_str.pop(end_index)
-        reversed_str = list_str[::-1]
-        print(list_str, reversed_str)
-        if list_str == reversed_str:
-            return end_index
-        else:
-            list_str.pop(start_index)
-            reversed_str = list_str[::-1]
-            if list_str == reversed_str:
-                return start_index
+        if next_index == str_len:
+            removed_index = prev_index
+            break
 
-        start_index += 1
-        end_index -= 1
+        prev_char = string_query[prev_index]
+        next_char = string_query[next_index]
 
-    return -1
+        if prev_char != next_char:
+            try:
+                check_prev = string_query[prev_index - 1]
+            except IndexError:
+                removed_index = next_index
+                break
+
+            try:
+                check_next = string_query[next_index + 1]
+            except IndexError:
+                removed_index = prev_index
+                break
+
+            if check_prev == next_char:
+                removed_index = prev_index
+                break
+            elif check_next == prev_char:
+                removed_index = next_index
+                break
+
+        next_index += 1
+        prev_index -= 1
+
+    return removed_index
 
 
+print(palindromeIndex("bcbc"))
 print(palindromeIndex("aaab"))
 print(palindromeIndex("baa"))
 print(palindromeIndex("aaa"))
+print("=================")
+# fmt:off
+print(palindromeIndex("quyjjdcgsvvsgcdjjyq"))
+print(palindromeIndex("hgygsvlfwcwnswtuhmyaljkqlqjjqlqkjlaymhutwsnwcflvsgygh"))
+print(palindromeIndex("fgnfnidynhxebxxxfmxixhsruldhsaobhlcggchboashdlurshxixmfxxxbexhnydinfngf"))
+print(palindromeIndex("bsyhvwfuesumsehmytqioswvpcbxyolapfywdxeacyuruybhbwxjmrrmjxwbhbyuruycaexdwyfpaloyxbcpwsoiqtymhesmuseufwvhysb"))
+print(palindromeIndex("fvyqxqxynewuebtcuqdwyetyqqisappmunmnldmkttkmdlnmnumppasiqyteywdquctbeuwenyxqxqyvf"))
+print(palindromeIndex("mmbiefhflbeckaecprwfgmqlydfroxrblulpasumubqhhbvlqpixvvxipqlvbhqbumusaplulbrxorfdylqmgfwrpceakceblfhfeibmm"))
+print(palindromeIndex("tpqknkmbgasitnwqrqasvolmevkasccsakvemlosaqrqwntisagbmknkqpt"))
+print(palindromeIndex("lhrxvssvxrhl"))
+print(palindromeIndex("prcoitfiptvcxrvoalqmfpnqyhrubxspplrftomfehbbhefmotfrlppsxburhyqnpfmqlaorxcvtpiftiocrp"))
+print(palindromeIndex("kjowoemiduaaxasnqghxbxkiccikxbxhgqnsaxaaudimeowojk"))
+print("=================")
+print(palindromeIndex("wykkttfghdvbyxbxefnltpnbdkkdbnptlnfexbxybvdhgfttkkyw"))
+print(palindromeIndex("iynilxchelphhsjiftgmbtaxlnbrbhsrptfxfddmhoerxaaaaxreohmddfxftprshbrbnlxatbmgtfishhplehcxlinyi"))
+print(palindromeIndex("xfsxrgylhgebvagwhmctvsbnqbqwwcpuhisbrtmbawsdyulxccxluydsabmtrbsihupcwwqbqnbsvtcmhwgavbeghlygrxsfx"))
+print(palindromeIndex("uaklbdlxfbvypmqselghnbmmwyywmmbnhglesqmpvbfxldblkau"))
+print(palindromeIndex("xthxhvykhasivjiwmbbmwivisahkyvhxhtx"))
+print(palindromeIndex("oladkyolbdmqwlkejjeklwmdbloykdalo"))
+print(palindromeIndex("kbkseyyvccgpqtqterkrrkretqtqpgccvyyskbk"))
+print(palindromeIndex("crwlgqeubhhvigsliydbuvvvvubdyilsgivhhbueqglwrc"))
+print(palindromeIndex("slwaebreoxussybaujipwtqdwlayumpeicdiifaafiidciepmuylwdqtwpijuabyssuxoerbeawls"))
+print(palindromeIndex("nujquecofwcbnfyaayfnbcwfceuqjun"))
+# fmt:on
